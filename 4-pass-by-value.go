cb7bda300e42d107941361e3f139330c0ac8bcbd
siddharth11 <siddharth2parmar@gmail.com>
2020-02-23 02:23:02 +0100

    Pass args by value

---
+++ 4-pass-by-value.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Siddharth"
+
+	fmt.Println("\nValue of \"name\" before update func:", name)
+
+	updateName(name)
+
+	fmt.Println("\nValue of \"name\" after update func:", name)
+}
+
+func updateName(name string) string {
+	name = "Muskan"
+
+	fmt.Println("\nValue of \"name\" inside update func:", name)
+
+	return name
+}
