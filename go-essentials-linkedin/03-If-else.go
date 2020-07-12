aae5bb7c93f5ac533d64b51cc57af3259fce9746
Siddharth Parmar <siddharthp@uber.com>
2020-07-12 20:19:39 +0200

    Try if else

---
+++ go-essentials-linkedin/03-If-else.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := 10
+
+	if x > 5 {
+		fmt.Println("x is big")
+	}
+
+	if x > 100 {
+		fmt.Println("x is very big")
+	} else {
+		fmt.Println("x is not that big")
+	}
+
+	if x > 5 && x < 15 {
+		fmt.Println("x is just right")
+	}
+
+	a := 11.0
+	b := 20.0
+
+	if frac := a / b; frac > 0.5 {
+		fmt.Printf("a (%v) is more than half of b (%v)\n", a, b)
+	}
+}
